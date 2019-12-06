416f4234874352e8bdcbeff1109fe590a8c2a550
hitenjain88 <hitenjain88@gmail.com>
2019-12-06 21:34:11 +0530

    msg from fetched

--- mail/mail.py
+++ mail/mail.py
@@ -1,23 +1,11 @@
 import email,imaplib
 
-From=[]
-subject=[]
 
 def get_credentials():
     username=input('Enter the Username : ')
     password=input('Enter the Password : ')
     return [username,password]
 
-
-def get_mail(index1):
-    typ,data=con.fetch(index1,'(RFC822)')
-    for word in data:
-        if isinstance(word,tuple):
-            msg=email.message_from_bytes(word[1])
-            email_subject=msg['subject']
-            email_from=msg['from']
-    return [email_subject,email_from,msg]
-            
 
 def make_connection(cred):
     con=imaplib.IMAP4_SSL('imap.gmail.com')
@@ -28,13 +16,17 @@ def make_connection(cred):
         typ, data=con.search(None,'ALL')
         index=data[0].split()
         for i in index[::-1]:
-            #mail_details=get_mail(i)
             typ,data=con.fetch(i,'(RFC822)')
             for word in data:
                 if isinstance(word,tuple):
                     msg=email.message_from_bytes(word[1])
                     email_subject=msg['subject']
                     email_from=msg['from']
+            for word in msg.walk():
+                if word.get_content_type()=='text/plain':
+                    body=part.get_payload(decode=True)
+                    msg1=body.decode('utf-8')
+            message.append(msg1)
             subject.append(email_subject)
             From.append(email_from)
     except Exception as e:
@@ -42,5 +34,8 @@ def make_connection(cred):
 
     
 if __name__=='__main__':
+    message=[]
+    From=[]
+    subject=[]
     credentials=get_credentials()
     make_connection(credentials)
